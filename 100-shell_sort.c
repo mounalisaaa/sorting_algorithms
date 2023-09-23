3046ee07ecbbc279021ec08932c2b4ef09eb973b
0xk1 <ultra.saidi6@gmail.com>
2023-09-23 14:01:35 +0100

    :)

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ *               using the Shell Sort algorithm.
+ *
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ *
+ */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap;
@@ -9,9 +17,7 @@ void shell_sort(int *array, size_t size)
 
 	gap = 1;
 	while (gap < size / 3)
-	{
 		gap = gap * 3 + 1;
-	}
 	while (gap > 0)
 	{
 		i = gap;
@@ -27,7 +33,7 @@ void shell_sort(int *array, size_t size)
 			array[j] = temp;
 			i++;
 		}
-		gap /= 3;
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
